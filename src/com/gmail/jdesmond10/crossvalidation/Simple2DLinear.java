ddb224cfbce640bc3cc8a87616d84feda2ab5d01
SkiingxMoose <jodesmond@live.com>
2015-06-01 23:45:57 -0400

    Finished Simple2DLinear

--- src/com/gmail/jdesmond10/crossvalidation/Simple2DLinear.java
+++ src/com/gmail/jdesmond10/crossvalidation/Simple2DLinear.java
@@ -2,16 +2,31 @@ package com.gmail.jdesmond10.crossvalidation;
 
 import java.util.function.Function;
 
+import org.ejml.simple.SimpleMatrix;
+
 public class Simple2DLinear implements RegressionAlgorithm {
 
 	@Override
 	public Function<Float, Float> generatePredictor(final int Complexity,
 			final LinearData data) {
-		// TODO 2DLinear
-
 		// Complexity is basically going to be ignored.
 
-		return null;
+		final SimpleMatrix x = data.getX();
+		final SimpleMatrix y = data.getY();
+
+		// A is given by (X^T * X)^-1 * (X^T * Y) where X^T is X transpose.
+		SimpleMatrix A = new SimpleMatrix(2, 1);
+
+		// y.mult(x.transpose());
+
+		x.transpose().mult(x).invert();
+
+		A = (((x.transpose()).mult(x)).invert()).mult(x.transpose().mult(y));
+
+		return new PolynomialFunction((float) A.get(0),(float) A.get(1),0f,0f,0f);
+
+
+
 	}
 
 }
